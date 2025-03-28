9c25facf43700b99a84c0fec68ba8824ae4642a4
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2025-03-28 14:23:59 -0300

    Schema da tabela de empresas

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { env } from '../env'
+import * as schema from './schema'
 
 const pg = postgres(env.DATABASE_URL)
-export const db = drizzle(pg)
+export const db = drizzle(pg, { schema })
