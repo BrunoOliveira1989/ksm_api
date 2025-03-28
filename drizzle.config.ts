9c14affbbc998b465bb546752ca62cb5f138e604
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2025-03-28 14:49:37 -0300

    Arquivo de seed e primeira migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit'
 import { env } from './src/env'
 
 export default defineConfig({
-  schema: './src/db/schemas/index.ts',
+  schema: './src/db/schema',
   out: './src/db/drizzle',
   dialect: 'postgresql',
   dbCredentials: {
