2183e3d7b2f7f2f0162ae8a4907c703b22158817
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2025-03-31 10:55:57 -0300

    Relação entre cliente e grupo de clientes

--- src/db/schema/customers-groups.ts
+++ src/db/schema/customers-groups.ts
@@ -1,6 +1,17 @@
+import { relations } from 'drizzle-orm'
 import { integer, pgTable, text } from 'drizzle-orm/pg-core'
+import { customers } from '.'
 
 export const customersGroups = pgTable('customers_groups', {
   id: integer('id').primaryKey(),
   description: text('description').notNull(),
 })
+
+export const customersGroupsRelations = relations(
+  customersGroups,
+  ({ many }) => {
+    return {
+      customers: many(customers),
+    }
+  }
+)
--- src/db/schema/customers.ts
+++ src/db/schema/customers.ts
@@ -1,4 +1,6 @@
+import { relations } from 'drizzle-orm'
 import { integer, pgTable, text } from 'drizzle-orm/pg-core'
+import { customersGroups } from '.'
 
 export const customers = pgTable('customers', {
   id: integer('id').primaryKey(),
@@ -6,6 +8,15 @@ export const customers = pgTable('customers', {
   tradeName: text('trade_name'),
   city: text('city'),
   state: text('state'),
-  groupId: integer('group_id'),
-  groupDescription: text('group_description'),
+  groupId: integer('group_id').references(() => customersGroups.id),
+})
+
+export const customersRelations = relations(customers, ({ one }) => {
+  return {
+    group: one(customersGroups, {
+      fields: [customers.groupId],
+      references: [customersGroups.id],
+      relationName: 'customer_group',
+    }),
+  }
 })
