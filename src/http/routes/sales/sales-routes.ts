7260159f0bbaed2c329a39ac4854f2fc9156b685
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2025-04-01 09:20:37 -0300

    Rota: obter vendas pro grupo de produtos

--- src/http/routes/sales/sales-routes.ts
+++ src/http/routes/sales/sales-routes.ts
@@ -1,8 +1,10 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { getProductSalesHistoryRoute } from './get-product-sales-history-route'
 import { getSalesByDaysOfTheLastWeekRoute } from './get-sales-by-days-of-the-last-week-route'
+import { getSalesByProductGroupRoute } from './get-sales-by-product-group-route'
 
 export const salesRoutes: FastifyPluginAsyncZod = async app => {
   await app.register(getProductSalesHistoryRoute)
   await app.register(getSalesByDaysOfTheLastWeekRoute)
+  await app.register(getSalesByProductGroupRoute)
 }
