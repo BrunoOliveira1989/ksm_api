722986d64176da89eaefbad844202ef98f79452c
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2025-04-07 17:27:05 -0300

    Rota: obter as 10 últimas vendas

--- src/http/routes/sales/sales-routes.ts
+++ src/http/routes/sales/sales-routes.ts
@@ -1,4 +1,5 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getLastTenSalesRoute } from './get-last-ten-sale-route'
 import { getProductSalesHistoryRoute } from './get-product-sales-history-route'
 import { getSalesByDaysOfTheLastWeekRoute } from './get-sales-by-days-of-the-last-week-route'
 import { getSalesByProductGroupRoute } from './get-sales-by-product-group-route'
@@ -7,4 +8,5 @@ export const salesRoutes: FastifyPluginAsyncZod = async app => {
   await app.register(getProductSalesHistoryRoute)
   await app.register(getSalesByDaysOfTheLastWeekRoute)
   await app.register(getSalesByProductGroupRoute)
+  await app.register(getLastTenSalesRoute)
 }
